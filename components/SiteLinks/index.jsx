c9adbefafd430edb00754c3f7b4790a9142c60a9
Aric Huang <arichuang@gmail.com>
2017-03-04 13:32:22 -0800

    fix link sizes on various images

--- components/SiteLinks/index.jsx
+++ components/SiteLinks/index.jsx
@@ -13,40 +13,40 @@ class SiteLinks extends React.Component {
           <ul>
             {config.siteTwitterUrl && (
               <li>
-                <a href={config.siteTwitterUrl}>
+                <a className="sitelink" href={config.siteTwitterUrl}>
                   <i className="fa fa-twitter" />
                 </a>
               </li>
             )}
             {config.siteGithubUrl && (
               <li>
-                <a href={config.siteGithubUrl}>
+                <a className="sitelink" href={config.siteGithubUrl}>
                   <i className="fa fa-github-alt" />
                 </a>
               </li>
             )}
             {config.siteLinkedInUrl && (
               <li>
-                <a href={config.siteLinkedInUrl}><i className="fa fa-linkedin" /></a>
+                <a className="sitelink" href={config.siteLinkedInUrl}><i className="fa fa-linkedin" /></a>
               </li>
             )}
             {config.siteFacebookUrl && (
               <li>
-                <a href={config.siteFacebookUrl}><i className="fa fa-facebook" /></a>
+                <a className="sitelink" href={config.siteFacebookUrl}><i className="fa fa-facebook" /></a>
               </li>
             )}
           </ul>
           <ul>
             {config.siteYoutubeUrl && (
               <li>
-                <a href={config.siteYoutubeUrl}>
+                <a className="sitelink" href={config.siteYoutubeUrl}>
                   <i className="fa fa-youtube-play" />
                 </a>
               </li>
             )}
             {config.siteSoundcloudUrl && (
               <li>
-                <a href={config.siteSoundcloudUrl}>
+                <a className="sitelink" href={config.siteSoundcloudUrl}>
                   <i className="fa fa-soundcloud" />
                 </a>
               </li>
@@ -55,14 +55,14 @@ class SiteLinks extends React.Component {
           <ul>
             {config.siteEmailUrl && (
               <li>
-                <a href={config.siteEmailUrl}>
+                <a className="sitelink" href={config.siteEmailUrl}>
                   <i className="fa fa-envelope-o" />
                 </a>
               </li>
             )}
             {config.siteTelegramUrl && (
               <li>
-                <a href={config.siteTelegramUrl}>
+                <a className="sitelink" href={config.siteTelegramUrl}>
                   <i className="fa fa-paper-plane" />
                 </a>
               </li>
@@ -71,7 +71,7 @@ class SiteLinks extends React.Component {
           <ul>
             {config.siteRssUrl && (
               <li>
-                <a href={config.siteRssUrl}><i className="fa fa-rss" /></a>
+                <a className="sitelink" href={config.siteRssUrl}><i className="fa fa-rss" /></a>
               </li>
             )}
           </ul>
